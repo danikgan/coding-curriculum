f036cfd563850ce649f4023fc6e335e4dc9bc75e
Gabriel Vanca <vanca.gabriel@gmail.com>
2016-01-31 22:44:05 +0000

    upd

--- Coding Curriculum/Assets/Scripts/NewMove1.cs
+++ Coding Curriculum/Assets/Scripts/NewMove1.cs
@@ -7,9 +7,14 @@ public class NewMove1 : MonoBehaviour {
 	Vector3 pos;                                // For movement
 	float speed = 2.0f;
 
+    public GameObject Map;
+    private Transform Obstacles;
+
 
 	void Start () {
 		pos = transform.position;          // Take the initial position
+	    Transform groundGameObject = Map.transform.Find("Ground");
+	    Obstacles = groundGameObject.Find("Orange_Box32x32");
 	}
 
 	void FixedUpdate () {
@@ -27,8 +32,15 @@ public class NewMove1 : MonoBehaviour {
 			pos += Vector3.down;
 		}
 		startPosition = transform.position;
+        //Vector3 endPos = transform.position + pos * Time.deltaTime * speed;
 
-		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+	    if (pos == startPosition)
+	        return;
+
+	    //var x = Obstacles.GetComponent<PolygonCollider2D>().bounds.Contains(pos);
+
+        if (!Obstacles.GetComponent<PolygonCollider2D>().bounds.Contains(pos))
+		    transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
 		print ("I move");
 		//if (transform.position != pos) {
 			//transform.position = Vector3.MoveTowards(transform.position, startPosition, Time.deltaTime * speed);
--- Coding Curriculum/Assets/Scripts/PlayerMovement.cs
+++ Coding Curriculum/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody2D rbody;
 	Animator anim;
+
+    private float speed = 2.0f;
 	// Use this for initialization
 	void Start () {
 		rbody = GetComponent<Rigidbody2D> ();
@@ -15,6 +17,7 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector2 movement_vector = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
 		if (movement_vector != Vector2.zero)
 		{
 			anim.SetBool("IsWalking", true);
@@ -26,7 +29,7 @@ public class PlayerMovement : MonoBehaviour {
 			anim.SetBool("IsWalking", false);
 		}
 
-		rbody.MovePosition (rbody.position + movement_vector * Time.deltaTime);
+		rbody.MovePosition (rbody.position + movement_vector * Time.deltaTime * speed);
 	
 
 	}
