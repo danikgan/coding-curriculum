093b45536ed381b58d8859986297c01fa9904899
Gabriel Vanca <vanca.gabriel@gmail.com>
2016-01-31 23:58:46 +0000

    Fixed movement

--- Coding Curriculum/Assets/Scripts/NewMove1.cs
+++ Coding Curriculum/Assets/Scripts/NewMove1.cs
@@ -5,7 +5,7 @@ public class NewMove1 : MonoBehaviour {
 	
 	Vector3 startPosition;
 	Vector3 pos;                                // For movement
-	float speed = 2.0f;
+	//float speed = 0.05f;
 
     public GameObject Map;
     private Transform Obstacles;
@@ -18,30 +18,50 @@ public class NewMove1 : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		if(Input.GetKey(KeyCode.A) && transform.position == pos) {        // Left
-			pos += Vector3.left;
-		}
-		if(Input.GetKey(KeyCode.D) && transform.position == pos) {        // Right
-			pos += Vector3.right;
-		}
-		if(Input.GetKey(KeyCode.W) && transform.position == pos) {        // Up
-			pos += Vector3.up;
 
-		}
-		if(Input.GetKey(KeyCode.S) && transform.position == pos) {        // Down
-			pos += Vector3.down;
-		}
-		startPosition = transform.position;
+        var rigidBody = GetComponent<Rigidbody2D>();
+
+        Vector2 movementVetor = new Vector2();
+
+        if (Input.GetKey(KeyCode.A)) {        // Left
+           // rigidBody.velocity = new Vector2((float) (-32* 0.032), 0);
+           movementVetor = new Vector2(-32 * 0.032f,0);
+        }
+		if(Input.GetKey(KeyCode.D)) {        // Right
+            //rigidBody.velocity = new Vector2((float) (32* 0.032), 0);
+            movementVetor = new Vector2(32 * 0.032f, 0);
+        }
+		if(Input.GetKey(KeyCode.W)) {        // Up
+            //rigidBody.velocity = new Vector2(0, (float)(32 * 0.032));
+            movementVetor = new Vector2(0, 32 * 0.032f);
+
+        }
+		if(Input.GetKey(KeyCode.S)) {        // Down
+          // rigidBody.velocity = new Vector2(0, (float) (-32 * 0.032));
+            movementVetor = new Vector2(0, -32 * 0.032f);
+        }
+		//startPosition = transform.position;
+        if(!movementVetor.Equals(new Vector2()))
+            rigidBody.MovePosition(rigidBody.position + movementVetor);
         //Vector3 endPos = transform.position + pos * Time.deltaTime * speed;
 
-	    if (pos == startPosition)
-	        return;
+        // if (pos == startPosition)
+        //     return;
 
-	    //var x = Obstacles.GetComponent<PolygonCollider2D>().bounds.Contains(pos);
+        //var x = Obstacles.GetComponent<PolygonCollider2D>().bounds.Contains(pos);
 
-        if (!Obstacles.GetComponent<PolygonCollider2D>().bounds.Contains(pos))
-		    transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
-		print ("I move");
+        //  if (!Obstacles.GetComponent<PolygonCollider2D>().bounds.Contains(pos))
+
+
+
+        //  float moveHorizontal = Input.GetAxis("Horizontal");
+        //    float moveVertical = Input.GetAxis("Vertical");
+
+        // rigidBody.velocity = new Vector2(Mathf.Ceil(moveHorizontal) * speed, Mathf.Ceil(moveVertical) * speed);
+
+
+        //transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        print ("I move");
 		//if (transform.position != pos) {
 			//transform.position = Vector3.MoveTowards(transform.position, startPosition, Time.deltaTime * speed);
 	//	}
