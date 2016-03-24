53ad6f848a932baba00c461d1a47aec4ed70f069
siberianbrother <mik.german@gmail.com>
2016-03-24 20:29:50 +0000

    Added main menu button to scenes

---
+++ Coding Curriculum/Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+	public GUIStyle style;
+		
+	void OnGUI () {
+
+		GUI.skin.button = style;
+
+		if (GUI.Button (new Rect (Screen.width*0.329f, Screen.height*0.06f, Screen.width*0.055f, Screen.width*0.055f), "")) {
+			SceneManager.LoadScene ("main_menu");
+		}
+	
+	}
+
+}
